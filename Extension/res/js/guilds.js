7c504c3aa11dbbc6fa014b66d463fb5612e4d427
Goodwine <goodwine@SOS>
2013-03-11 01:45:41 -0600

    names now read and parsed
    names now read and parsed, next step is setting attributes so they can
    be shown/hid, will also need to set lvl/voc to rows, online indicator
    later.

--- Extension/res/js/guilds.js
+++ Extension/res/js/guilds.js
@@ -8,15 +8,18 @@
 });*/
 
 server = document.getElementsByTagName('body')[0].innerHTML.match(/The guild was founded on (.*?) on/);
+characters = [];
+header = null;
+
 if(server != null) {
   server = server[1];
   loadCharacters(server);
 }
 
 function loadCharacters(server) {
-  chrome.extension.sendMessage({action: 'whois', server: server, htmlDecode: true}, function (response) {
+  chrome.extension.sendMessage({action: 'whois', server: server}, function (response) {
     if(response.tryAgain === false) {
-      setOnlineStatus(response.playersOnline, response.htmlDecode);
+      setOnlineStatus(response.playersOnline);
     } else {
       setTimeout(function(){loadCharacters(server);}, 1000);
     }
@@ -24,6 +27,39 @@ function loadCharacters(server) {
 }
 
 function setOnlineStatus(playersOnline, htmlDecode) {
-	console.log(htmlDecode);
-	console.log(playersOnline);
+	if(characters.length == 0) {
+    function htmlDecode (input) {
+      if(/[<>]/.test(input)) {
+        return "Invalid Input";
+      }
+      var e = document.createElement('div');
+      e.innerHTML = input;
+      return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+    }
+    var memberList = document.getElementsByTagName('table');
+    var foundMembers = false;
+    for(var i = 0; i < memberList.length; i++) {
+      if (/<b>Guild Members<\/b>/.test(memberList[i].innerHTML)) {
+        memberList = memberList[i].getElementsByTagName('tr');
+        foundMembers = memberList.length > 0;
+        break;
+      }
+    }
+    if (foundMembers) {
+      var characterNameRegex = /<a.*?href.*?subtopic=characters.*?>(.*?)<\/a>/i;
+      header = memberList[0];
+      var rank = null;
+      for(var j = 2; j < memberList.length; j++) {
+        var tempRank = memberList[j].getElementsByTagName('td')[0].childNodes[0].nodeValue.trim();
+        if(tempRank.length > 0)
+          rank = tempRank;
+        characters.push({
+          'name': htmlDecode(characterNameRegex.exec(memberList[j].innerHTML)[1]),
+          'row': memberList[j],
+          'rank': rank
+        });
+      }
+    }
+	}
+	console.log(characters);
 }
