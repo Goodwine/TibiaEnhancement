ca224be54108d7baca2d764a011a64edb720a909
Goodwine <goodwine@SOS>
2013-03-12 01:25:14 -0600

    added header text to guild pages
    added header text to guild pages, moved out function, stupid mistake of
    forgetting to remove a parameter and causing it always to be overriden
    locally and not finding the function, shame

--- Extension/res/js/guilds.js
+++ Extension/res/js/guilds.js
@@ -10,15 +10,33 @@
 server = document.getElementsByTagName('body')[0].innerHTML.match(/The guild was founded on (.*?) on/);
 characters = [];
 header = null;
+headerSpan = null;
 
-if(server != null) {
+if (server != null) {
   server = server[1];
   loadCharacters(server);
 }
 
+function htmlDecode (input) {
+  if (/[<>]/.test(input)) {
+    return "Invalid Input";
+  }
+  var e = document.createElement('div');
+  e.innerHTML = input;
+  return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue.replace(/\s/g, ' ');
+}
+
+function createLvlvoc(parent) {
+  var lvlvoc = document.createElement('span');
+  lvlvoc.style.fontSize = 'xx-small';
+  lvlvoc.style.float = 'right';
+  parent.appendChild(lvlvoc);
+  return lvlvoc;
+}
+
 function loadCharacters(server) {
   chrome.extension.sendMessage({action: 'whois', server: server}, function (response) {
-    if(response.tryAgain === false) {
+    if (response.tryAgain === false) {
       setOnlineStatus(response.playersOnline);
     } else {
       setTimeout(function(){loadCharacters(server);}, 1000);
@@ -26,19 +44,11 @@ function loadCharacters(server) {
   })
 }
 
-function setOnlineStatus(playersOnline, htmlDecode) {
-  if(characters.length == 0) {
-    function htmlDecode (input) {
-      if(/[<>]/.test(input)) {
-        return "Invalid Input";
-      }
-      var e = document.createElement('div');
-      e.innerHTML = input;
-      return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue.replace(/\s/g, ' ');
-    }
+function setOnlineStatus(playersOnline) {
+  if (characters.length == 0) {
     var memberList = document.getElementsByTagName('table');
     var foundMembers = false;
-    for(var i = 0; i < memberList.length; i++) {
+    for (var i = 0; i < memberList.length; i++) {
       if (/<b>Guild Members<\/b>/.test(memberList[i].innerHTML)) {
         memberList = memberList[i].getElementsByTagName('tr');
         foundMembers = memberList.length > 0;
@@ -47,37 +57,43 @@ function setOnlineStatus(playersOnline, htmlDecode) {
     }
     if (foundMembers) {
       var characterNameRegex = /<a.*?href.*?subtopic=characters.*?>(.*?)<\/a>/i;
-      header = memberList[0];
+      header = memberList[0].getElementsByTagName('td')[0];
       var rank = null;
-      for(var i = 2; i < memberList.length; i++) {
+      for (var i = 2; i < memberList.length; i++) {
         var tempRank = memberList[i].getElementsByTagName('td')[0].childNodes[0].nodeValue.trim();
-        if(tempRank.length > 0)
+        if (tempRank.length > 0)
           rank = tempRank;
         characters.push({
           'name': htmlDecode(characterNameRegex.exec(memberList[i].innerHTML)[1]),
           'row': memberList[i],
-          'rank': rank
+          'rank': rank,
+          'ranke': null,
+          'lvlvoc': null,
+          'indicator': null
         });
       }
     }
   }
+  var totalOnline = 0;
   for (var i = 0; i < characters.length; i++) {
     var cells = characters[i].row.getElementsByTagName('td');
-    var lvlvoc = cells[0].getElementsByClassName('lvlvoc');
-    lvlvoc = lvlvoc.length > 0 ? lvlvoc[0] : null;
-    if (!lvlvoc && playersOnline[characters[i].name]) {
-      lvlvoc = document.createElement('span');
-      lvlvoc.className = 'lvlvoc';
-      lvlvoc.style.fontSize = 'xx-small';
-      lvlvoc.style.float = 'right';
-      cells[0].appendChild(lvlvoc);
-    }
+    if (!characters[i].lvlvoc)
+      characters[i].lvlvoc = createLvlvoc(cells[0]);
     if (playersOnline[characters[i].name]) {
+      totalOnline++;
       var player = playersOnline[characters[i].name];
-      lvlvoc.style.display = '';
-      lvlvoc.innerText = 'Lv:' + player.lvl + ' Voc:' + player.vocShort;
-    } else if (lvlvoc) {
-      lvlvoc.style.display = 'none';
+      characters[i].lvlvoc.style.display = '';
+      characters[i].lvlvoc.innerText = 'Lv:' + player.lvl + ' Voc:' + player.vocShort;
+    } else {
+      characters[i].lvlvoc.style.display = 'none';
     }
   }
+  if (!headerSpan) {
+    headerSpan = document.createElement('span');
+    headerSpan.style.fontSize = 'xx-small';
+    headerSpan.style.paddingRight = '5px';
+    headerSpan.style.float = 'right';
+    header.appendChild(headerSpan);
+  }
+  headerSpan.innerHTML =  'Guild Members: ' + characters.length + ' (Online: ' + totalOnline + ') &nbsp; Online on ' + server + ': ' + Object.keys(playersOnline).length;
 }
