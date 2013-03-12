d9a723f38256361cb3cf3be4fbc6a13cdce469dd
Goodwine <goodwine@SOS>
2013-03-12 00:50:40 -0600

    Now Adds lvl voc to a guild page
    Now Adds lvl voc to a guild page.
    Hidding, indicator and header still missing, need to move styles to a
    CSS maybe

--- Extension/res/js/guilds.js
+++ Extension/res/js/guilds.js
@@ -1,7 +1,7 @@
 /** This is a content script to get a list from the online characters supposedly stored locally.
 
-	This content script is supposed to work only in http://www.tibia.com/news/?subtopic=latestnews
-	and in http://www.tibia.com/news/?subtopic=newsarchive */
+  This content script is supposed to work only in http://www.tibia.com/news/?subtopic=latestnews
+  and in http://www.tibia.com/news/?subtopic=newsarchive */
 
 /*chrome.extension.sendMessage({action:'whois', server: 'Danera'}, function (response) {
   console.log(response);
@@ -27,14 +27,14 @@ function loadCharacters(server) {
 }
 
 function setOnlineStatus(playersOnline, htmlDecode) {
-	if(characters.length == 0) {
+  if(characters.length == 0) {
     function htmlDecode (input) {
       if(/[<>]/.test(input)) {
         return "Invalid Input";
       }
       var e = document.createElement('div');
       e.innerHTML = input;
-      return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+      return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue.replace(/\s/g, ' ');
     }
     var memberList = document.getElementsByTagName('table');
     var foundMembers = false;
@@ -49,17 +49,35 @@ function setOnlineStatus(playersOnline, htmlDecode) {
       var characterNameRegex = /<a.*?href.*?subtopic=characters.*?>(.*?)<\/a>/i;
       header = memberList[0];
       var rank = null;
-      for(var j = 2; j < memberList.length; j++) {
-        var tempRank = memberList[j].getElementsByTagName('td')[0].childNodes[0].nodeValue.trim();
+      for(var i = 2; i < memberList.length; i++) {
+        var tempRank = memberList[i].getElementsByTagName('td')[0].childNodes[0].nodeValue.trim();
         if(tempRank.length > 0)
           rank = tempRank;
         characters.push({
-          'name': htmlDecode(characterNameRegex.exec(memberList[j].innerHTML)[1]),
-          'row': memberList[j],
+          'name': htmlDecode(characterNameRegex.exec(memberList[i].innerHTML)[1]),
+          'row': memberList[i],
           'rank': rank
         });
       }
     }
-	}
-	console.log(characters);
+  }
+  for (var i = 0; i < characters.length; i++) {
+    var cells = characters[i].row.getElementsByTagName('td');
+    var lvlvoc = cells[0].getElementsByClassName('lvlvoc');
+    lvlvoc = lvlvoc.length > 0 ? lvlvoc[0] : null;
+    if (!lvlvoc && playersOnline[characters[i].name]) {
+      lvlvoc = document.createElement('span');
+      lvlvoc.className = 'lvlvoc';
+      lvlvoc.style.fontSize = 'xx-small';
+      lvlvoc.style.float = 'right';
+      cells[0].appendChild(lvlvoc);
+    }
+    if (playersOnline[characters[i].name]) {
+      var player = playersOnline[characters[i].name];
+      lvlvoc.style.display = '';
+      lvlvoc.innerText = 'Lv:' + player.lvl + ' Voc:' + player.vocShort;
+    } else if (lvlvoc) {
+      lvlvoc.style.display = 'none';
+    }
+  }
 }
